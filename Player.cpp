8de5d5f478fbddee96a30210650903aed0f8ada3
MoniqueDemers <monique.demers@uvm.edu>
2018-11-16 14:29:28 -0500

    Created Player class

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by moniq on 11/16/2018.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,27 @@
+//
+// Created by moniq on 11/16/2018.
+//
+
+#ifndef GRAPHICS_STARTER_PLAYER_H
+#define GRAPHICS_STARTER_PLAYER_H
+#include <bits/stdc++.h>
+#include "Piece.h"
+using namespace std;
+
+class Player {
+private:
+    string name;
+    int score;
+    vector<Piece> color1;
+    vector<Piece> color2;
+
+public:
+    //default constructor
+    Player();
+
+
+
+};
+
+
+#endif //GRAPHICS_STARTER_PLAYER_H
